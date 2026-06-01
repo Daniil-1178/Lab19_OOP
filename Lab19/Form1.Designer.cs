24cf2f3f1f1202382fa130997539143109fd8014
Daniil-1178 <daniilzubkov278@gmail.com>
2026-06-01 10:17:52 +0300

    Add files via upload

---
+++ Lab19/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Lab19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetInfo = new System.Windows.Forms.Button();
+            this.lstHardwareInfo = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Location = new System.Drawing.Point(85, 12);
+            this.btnGetInfo.Name = "btnGetInfo";
+            this.btnGetInfo.Size = new System.Drawing.Size(165, 23);
+            this.btnGetInfo.TabIndex = 0;
+            this.btnGetInfo.Text = "Отримати інформацію про ПК";
+            this.btnGetInfo.UseVisualStyleBackColor = true;
+            this.btnGetInfo.Click += new System.EventHandler(this.btnGetInfo_Click);
+            // 
+            // lstHardwareInfo
+            // 
+            this.lstHardwareInfo.FormattingEnabled = true;
+            this.lstHardwareInfo.Location = new System.Drawing.Point(12, 55);
+            this.lstHardwareInfo.Name = "lstHardwareInfo";
+            this.lstHardwareInfo.Size = new System.Drawing.Size(338, 316);
+            this.lstHardwareInfo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstHardwareInfo);
+            this.Controls.Add(this.btnGetInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetInfo;
+        private System.Windows.Forms.ListBox lstHardwareInfo;
+    }
+}
+
---
+++ Lab19/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Windows.Forms;
+
+namespace Lab19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetInfo_Click(object sender, EventArgs e)
+        {
+            lstHardwareInfo.Items.Clear();
+
+            OutputResult("ПРОЦЕСОР:", GetHardwareInfo("Win32_Processor", "Name"));
+            OutputResult("Виробник:", GetHardwareInfo("Win32_Processor", "Manufacturer"));
+            OutputResult("Опис:", GetHardwareInfo("Win32_Processor", "Description"));
+
+            OutputResult("ВІДЕОКАРТА:", GetHardwareInfo("Win32_VideoController", "Name"));
+            OutputResult("Відеопроцесор:", GetHardwareInfo("Win32_VideoController", "VideoProcessor"));
+            OutputResult("Версія драйверу:", GetHardwareInfo("Win32_VideoController", "DriverVersion"));
+            OutputResult("Об'єм пам'яті (байт):", GetHardwareInfo("Win32_VideoController", "AdapterRAM"));
+
+            OutputResult("ЖОРСТКИЙ ДИСК:", GetHardwareInfo("Win32_DiskDrive", "Caption"));
+            OutputResult("Об'єм диска (байт):", GetHardwareInfo("Win32_DiskDrive", "Size"));
+
+            OutputResult("МАТЕРИНСЬКА ПЛАТА:", GetHardwareInfo("Win32_BaseBoard", "Product"));
+            OutputResult("Виробник плати:", GetHardwareInfo("Win32_BaseBoard", "Manufacturer"));
+            OutputResult("Серійний номер плати:", GetHardwareInfo("Win32_BaseBoard", "SerialNumber"));
+
+            OutputResult("СИСТЕМА BIOS:", GetHardwareInfo("Win32_BIOS", "Caption"));
+            OutputResult("Виробник BIOS:", GetHardwareInfo("Win32_BIOS", "Manufacturer"));
+            OutputResult("Версія BIOS:", GetHardwareInfo("Win32_BIOS", "Version"));
+
+            OutputResult("МЕРЕЖЕВЕ ОБЛАДНАННЯ:", GetHardwareInfo("Win32_NetworkAdapter", "Name"));
+            OutputResult("Тип адаптера:", GetHardwareInfo("Win32_NetworkAdapter", "AdapterType"));
+            OutputResult("MAC-адреса:", GetHardwareInfo("Win32_NetworkAdapter", "MACAddress"));
+        }
+
+        private List<string> GetHardwareInfo(string WIN32_Class, string ClassItemField)
+        {
+            List<string> result = new List<string>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + WIN32_Class);
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                if (obj[ClassItemField] != null)
+                {
+                    result.Add(obj[ClassItemField].ToString().Trim());
+                }
+            }
+
+            return result;
+        }
+
+        private void OutputResult(string info, List<string> result)
+        {
+            if (result.Count > 0)
+            {
+                if (info.StartsWith("---"))
+                {
+                    lstHardwareInfo.Items.Add("");
+                    lstHardwareInfo.Items.Add(info);
+                }
+                else
+                {
+                    for (int i = 0; i < result.Count; i++)
+                    {
+                        lstHardwareInfo.Items.Add(info + " " + result[i]);
+                    }
+                }
+            }
+        }
+    }
+}
